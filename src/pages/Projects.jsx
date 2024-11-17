f2c7665f7e626ad750179f0de3ddc49921bbffa9
scdsajid02 <sajidscd02@gmail.com>
2024-11-17 22:33:46 +0530

    todo

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import canvax from "../img/canvax.png";
+import todo from "../img/todo.png"
 
 const Projects = () => {
   return (
@@ -10,17 +11,30 @@ const Projects = () => {
       <h1 className="text-center mb-4 text-l">
         I have created these webapplicatiopns using{" "}
         <span className="font-extrabold">React</span> and for Styling I have
-        used <span className="font-black">Tailwind Css</span>.Click on projects to view.
+        used <span className="font-black">Tailwind Css</span>.Click on projects
+        to view.
       </h1>
       <div>
-        <div className=" p-2 flex items-center flex-col md:flex-row gap-2 justify-around ">
+        <div className=" p-2 flex flex-wrap items-center flex-col md:flex-row gap-2 justify-around ">
           <a
             href="https://canvaxschools.netlify.app/"
             target="_blank "
             className="flex flex-col justify-center items-center border-2 border-red-400 p-2"
           >
             <img src={canvax} alt="" srcset="" className="w-56 h-56" />
-            <p className="underline text-blue-600 hover:text-red-600">Canvax Schools</p>
+            <p className="underline text-blue-600 hover:text-red-600">
+              Canvax Schools
+            </p>
+          </a>
+          <a
+            href="https://todofuture.netlify.app/"
+            target="_blank "
+            className="flex flex-col justify-center items-center border-2 border-red-400 p-2"
+          >
+            <img src={todo} alt="" srcset="" className="w-56 h-56" />
+            <p className="underline text-blue-600 hover:text-red-600">
+              Todo List
+            </p>
           </a>
         </div>
       </div>
